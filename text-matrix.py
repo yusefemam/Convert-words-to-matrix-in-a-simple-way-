094849ca52b79bc26f9863c7c6adddfe352ec1d3
yusefemam <emamyoussef49@gmail.com>
2024-12-03 05:04:54 +0200

    Update text-matrix.py

--- text-matrix.py
+++ text-matrix.py
@@ -1,7 +1,15 @@
 import re
+from collections import Counter
 
-text = input("Enter a text: ").strip()
-matrix = [[word] for word in re.findall(r'\b\w+\b', text.lower())]
-print("Text Matrix:")
-for row in matrix:
-    print(row)
+text = input("Enter a text: ").strip().lower()
+words = re.findall(r'\b\w+\b', text.lower().strip())
+
+print("Text matrix:")
+for _ in words:
+    print([_], end = " " *2)
+
+counts = Counter(words)
+
+print("\nWord counts:")
+for word, count in counts.items():
+    print(f"{word}: {count}")
